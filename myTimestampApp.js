b2772175326189d19bc87919073041d6a9501d3c
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-11-22 17:43:55 +0100

    added route to handle empty date param for now

--- myTimestampApp.js
+++ myTimestampApp.js
@@ -8,3 +8,12 @@ app.use('/public', express.static(__dirname + '/public'))
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
+
+app.get('/api/:date', (req, res) => {
+    const input = req.params.date
+    let output;
+    if (!input) output = new Date().toString()
+    input
+}, (req, res) => {
+    res.json({"utc": output})
+})
