8ac40350ad0de6709ad2443fc945b92c3825f30e
Jonathan Hurtig <jonathanhurtig@gmail.com>
2021-11-26 06:51:26 +0100

    altered function to convert utc input to UNIX

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.get('/api/:date?', (req, res) => {
     let utc = new Date(unix).toUTCString()
     res.json({ unix, utc })
   } else {
-    let unix = new Date(date).getTime()
+    let unix = Date.parse(date).getTime()
     if (isNaN(unix)) {
       res.json({ error: 'Invalid Date' })
     }
